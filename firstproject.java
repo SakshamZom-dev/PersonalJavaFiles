f1939e21a7b923fa4baa12e63c28ef9ab610e54d
Saksham Upadhyay <sakshamupadhyay.dev@gmail.com>
2026-01-16 15:37:09 +0530

    Added Meaning in the firstproject file

--- firstproject.java
+++ firstproject.java
@@ -1,13 +1,30 @@
+import java.util.Scanner;
+
 public class firstproject {
     
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
+        Scanner sc = new Scanner(System.in);
 
-        int a = 10;
-        int b = 20;
-        int c = a + b;
+        char grade = 'A';
+        String name = "Zom";
 
-        System.out.println(c + "is the answer");
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+        
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
+
+        int sum = num1 + num2;
+
+        boolean isEven = (sum % 2 == 0);
+
+        System.out.println("\n--- Results ---");
+        System.out.println("Hello, " + name + "!");
+        System.out.println("The sum of your numbers is: " + sum);
+        System.out.println("Is the sum even? " + isEven);
+        System.out.println("Your current grade is: " + grade);
+
+        sc.close();
     }
 }
